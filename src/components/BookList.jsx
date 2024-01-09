30368f8fb10e25efe206b53b117b54c11f62e1dc
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-01-09 14:53:11 +0100

    manca ancora solo add comment

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,62 +1,64 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
-import CommentArea from './CommentArea'
+import { useState } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    searchQuery: '',
-    selectedBook: null,
-  }
+const BookList = (props) => {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [asin, setAsin] = useState(null);
 
-  changeSelectedBook = (asin) => {
-    this.setState({
-      selectedBook: asin,
-    })
-  }
+	const changeSelectedBook = (asin) => {
+		setAsin(asin);
+	};
 
-  render() {
-    return (
-      <>
-        <Row>
-          <Col md={8}>
-            <Row className="justify-content-center mt-5">
-              <Col xs={12} md={4} className="text-center">
-                <Form.Group>
-                  <Form.Control
-                    type="search"
-                    placeholder="Cerca un libro"
-                    value={this.state.searchQuery}
-                    onChange={(e) =>
-                      this.setState({ searchQuery: e.target.value })
-                    }
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="g-2 mt-3">
-              {this.props.books
-                .filter((b) =>
-                  b.title.toLowerCase().includes(this.state.searchQuery)
-                )
-                .map((b) => (
-                  <Col xs={12} md={4} key={b.asin}>
-                    <SingleBook
-                      book={b}
-                      selectedBook={this.state.selectedBook}
-                      changeSelectedBook={this.changeSelectedBook}
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedBook} />
-          </Col>
-        </Row>
-      </>
-    )
-  }
-}
+	return (
+		<>
+			<Row>
+				<Col md={8}>
+					<Row className="justify-content-center mt-5">
+						<Col
+							xs={12}
+							md={4}
+							className="text-center"
+						>
+							<Form.Group>
+								<Form.Control
+									type="search"
+									placeholder="Cerca un libro"
+									value={searchQuery}
+									onChange={(e) =>
+										setSearchQuery(e.target.value)
+									}
+								/>
+							</Form.Group>
+						</Col>
+					</Row>
+					<Row className="g-2 mt-3">
+						{props.books
+							.filter((b) =>
+								b.title.toLowerCase().includes(searchQuery)
+							)
+							.map((b) => (
+								<Col
+									xs={12}
+									md={4}
+									key={b.asin}
+								>
+									<SingleBook
+										book={b}
+										selectedBook={asin}
+										changeSelectedBook={changeSelectedBook}
+									/>
+								</Col>
+							))}
+					</Row>
+				</Col>
+				<Col md={4}>
+					<CommentArea asin={asin} />
+				</Col>
+			</Row>
+		</>
+	);
+};
 
-export default BookList
+export default BookList;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,85 +1,58 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { useEffect, useState } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+import { token } from "../data/token";
+const CommentArea = (props) => {
+	const [comments, setComments] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: false,
-    isError: false,
-  }
+	const fetchingData = async () => {
+		setIsLoading(true);
+		try {
+			let response = await fetch(
+				"https://striveschool-api.herokuapp.com/api/comments/" +
+					props.asin,
+				{
+					headers: {
+						Authorization: token,
+					},
+				}
+			);
+			console.log(response);
+			if (response.ok) {
+				let comments = await response.json();
+				console.log(comments);
+				setIsLoading(false);
+				setIsError(false);
+				setComments(comments);
+			} else {
+				setIsLoading(false);
+				setIsError(true);
+			}
+		} catch (error) {
+			console.log(error);
+			setIsLoading(false);
+			setIsError(true);
+		}
+	};
 
-  // componentDidMount = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       'https://striveschool-api.herokuapp.com/api/comments/' +
-  //         this.props.asin,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Bearer inserisci-qui-il-tuo-token',
-  //         },
-  //       }
-  //     )
-  //     console.log(response)
-  //     if (response.ok) {
-  //       let comments = await response.json()
-  //       this.setState({ comments: comments, isLoading: false, isError: false })
-  //     } else {
-  //       console.log('error')
-  //       this.setState({ isLoading: false, isError: true })
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //     this.setState({ isLoading: false, isError: true })
-  //   }
-  // }
+	useEffect(() => {
+		if (props.asin) {
+			fetchingData();
+		}
+	}, [props.asin]);
 
-  componentDidUpdate = async (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
-      this.setState({
-        isLoading: true,
-      })
-      try {
-        let response = await fetch(
-          'https://striveschool-api.herokuapp.com/api/comments/' +
-            this.props.asin,
-          {
-            headers: {
-              Authorization: 'Bearer inserisci-qui-il-tuo-token',
-            },
-          }
-        )
-        console.log(response)
-        if (response.ok) {
-          let comments = await response.json()
-          this.setState({
-            comments: comments,
-            isLoading: false,
-            isError: false,
-          })
-        } else {
-          this.setState({ isLoading: false, isError: true })
-        }
-      } catch (error) {
-        console.log(error)
-        this.setState({ isLoading: false, isError: true })
-      }
-    }
-  }
+	return (
+		<div className="text-center">
+			{isLoading && <Loading />}
+			{isError && <Error />}
+			{/* <AddComment asin={props.asin} /> */}
+			<CommentList commentsToShow={comments} />
+		</div>
+	);
+};
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    )
-  }
-}
-
-export default CommentArea
+export default CommentArea;
